babdd44239cd858e0e2c1eadcb29c2ddd1206f56
Prabesh Aryal <mailforprabesh@gmail.com>
2026-07-13 09:23:35 +0545

    updated

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+//Input method
+console.log(prompt("enter your name"))//yele string ma matra show garxa number ligdena
+console.log(typeof(prompt("Enter your Name")))
+console.log(parseInt(prompt("enter Your Name")))
+console.log(parseFloat(prompt("enter Your Name")))
+console.log(prompt("enter Your Name","Hero"))
+
+
+
+//output method
+console.log("hello world..")
+console.log("Error occured");
+console.table(
+    {
+        "a":"123","b":"33"
+    }
+
+)
+
+console.assert(5==4,  "condition is not true")//prints only when the value is not equal to eachother
+
+//alert("hello m da f ka");
+
+//confirm("Are you gay..");//yaha cancell ra ok button aauxa
+
+//document.write("this is printed in the webPage...");//if hamlai page ma dekhauna xa bhani yo use garni
