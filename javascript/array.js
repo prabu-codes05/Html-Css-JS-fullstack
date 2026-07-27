a0e904a27fcf91c03b940798827916557ae161c1
Prabesh Aryal <mailforprabesh@gmail.com>
2026-07-27 14:05:43 +0545

    updated

--- javascript/array.js
+++ javascript/array.js
@@ -53,4 +53,76 @@
 
     const lastindex = [3,5,6,2,4,5,6,7,3,2,4,5,6,3].lastIndexOf(6)
     console.log(lastindex)
+
+    const arry =[12,3,4,5,6,11,24,4,55]
+    console.log(arry.sort((a,b)=>{return b-a}));
+
+    const arry1 = [12,,3,4,5,6,11,23,4,22]
+    console.log(arry1.reverse());
+
+    //=================Array iteration Method===========================//
+
+    const arr11 = [9,32,4,5,67,7,8,85,6,343,54]
+
+// forEach
+// it doensot create a new array 
+// it makes changes on the existing array 
+ console.log(arr11.forEach((val, idx, arr) =>{
+    console.log( val * 2)
+    // console.log(idx)
+    // console.log(arr)
+ })
+)
+
+
+// map 
+// it creates a new array
+// it doensot make changes on the existing array
+// it uses 3 paramters ( value, index, array)
+
+const newAr = arr11.map((val, idx, arr)=>{
+    // console.log(arr)
+     return val > 10
+    // console.log(idx)
+})
+console.log(newAr)
+
+// filter 
+// it creates a new array
+// it doensot make changes on the existing array
+// it uses 3 paramters ( value, index, array)
+// it gives the filter value 
+const newArs = arr11.filter((val, idx, arr)=>{
+    // console.log(arr)
+     return val > 10
+    // console.log(idx)
+})
+console.log(newArs)
+
+//reduce 
+const total = arr11.reduce((acc, total) => {//acc bhaneko acumulator ho that means shuruko value
+return acc-total
+})
+console.log(total)
+
+//right
+
+const totals = arr.reduceRight((acc,total) =>{
+    return acc - total
+})
+console.log(totals)
+
+
+//find
+const fruits = ["apple","orange","banana","strawberry","mango"]
+const isThere = fruits.find((val)=>{
+    return val === "strawberry"
+})
+console.log(isThere)
+
+//flat
+
+const myArr = [1,2,[3,4]]
+console.log(myArr.flat())
+
     
---
+++ javascript/lovecalculator.js
@@ -0,0 +1,5 @@
+prompt("what is your name");
+prompt("what is your gf name");
+var loveScore = Math.random()*100 //*100 garesi balla 0 dekhi 99 samma dinxa value natra 0 dekhi 9 samma matra dinxa normally
+loveScore = Math.floor(loveScore);//whole number ma dinxa yesle paxadi point hatayera
+alert("your love score is " + loveScore + "%");
